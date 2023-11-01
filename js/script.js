55d47dffb59ac29cf60e88c11748ab0af6c0ae86
Cameron <guilhermecameron@Air-de-Cameron.bbrouter>
2023-11-01 00:25:14 -0300

    correcao prevEl

--- js/script.js
+++ js/script.js
@@ -8,6 +8,6 @@ var swiper = new Swiper(".mySwiper", {
     },
     navigation: {
         nextEl: ".swiper-button-next",
-        prevEl: ".swiper.button-prev",
+        prevEl: ".swiper-button-prev",
     }
 });
